fd61d573e76e346b66a6ac198fe35daf09fc1739
Chris Menkhus <menkhus.chris@gmail.com>
2020-04-14 07:51:21 -0600

    Updates

---
+++ precache-manifest.14f1b956f9a709b641654ef4caf9d8ba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a987df327d88fd22c661474b7693cc1",
+    "url": "/DungeonHero/index.html"
+  },
+  {
+    "revision": "fae775e3d034a3febdda",
+    "url": "/DungeonHero/static/css/main.f80f3e5b.chunk.css"
+  },
+  {
+    "revision": "70bc6f4e505503fdb168",
+    "url": "/DungeonHero/static/js/2.921267a3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/DungeonHero/static/js/2.921267a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae775e3d034a3febdda",
+    "url": "/DungeonHero/static/js/main.7b10d7b5.chunk.js"
+  },
+  {
+    "revision": "2769ad37f60677e94d4c",
+    "url": "/DungeonHero/static/js/runtime-main.ad191d38.js"
+  },
+  {
+    "revision": "78763adf65c094d7ddc10d22950ba45c",
+    "url": "/DungeonHero/static/media/bg.78763adf.jpg"
+  },
+  {
+    "revision": "2a605915117463cb8fd73e6cdebe6325",
+    "url": "/DungeonHero/static/media/combat.2a605915.svg"
+  },
+  {
+    "revision": "0e35cd52ac891db4bd0eee8af2eeedcd",
+    "url": "/DungeonHero/static/media/details.0e35cd52.svg"
+  },
+  {
+    "revision": "1fa1edcad51429dd5c821e5310c68b0f",
+    "url": "/DungeonHero/static/media/equipment.1fa1edca.svg"
+  },
+  {
+    "revision": "700238a99c54e84663549d9098509624",
+    "url": "/DungeonHero/static/media/skills.700238a9.svg"
+  }
+]);
